4217cc3d7633aea3f68a6f81ec995be1c687b202
Benjamin Denham <ben.denham@gmail.com>
2015-09-08 18:15:06 +1200

    Add capability to force player data refresh in development.

--- src/lol_achievements/web/routes/player.clj
+++ src/lol_achievements/web/routes/player.clj
@@ -2,14 +2,18 @@
   (:require [compojure.core :refer [defroutes GET]]
             [ring.util.http-response :refer [ok]]
             [lol-achievements.common.data-store :refer [get-player-data]]
-            [lol-achievements.common.message-queue :refer [publish-player-data-request]]))
+            [lol-achievements.common.message-queue :refer [publish-player-data-request]]
+            [environ.core :refer [env]]))
 
-(defn get-player [name]
-  (if-let [player-data (get-player-data name)]
-    (ok player-data)
-    (do
-      (publish-player-data-request name)
-      (ok {}))))
+(defn get-player [req]
+  (let [name (get-in req [:params :name])
+        refresh? (nil? (get-in req [:params :refresh]))
+        player-data (get-player-data name)]
+    ;; Force a refresh if there is no stored player data, or if the
+    ;; site is in dev mode and a refresh parameter was provided.
+    (when (or (and refresh? (:dev env)) (nil? player-data))
+      (publish-player-data-request name))
+    (ok (or player-data {}))))
 
 (defroutes player-routes
-  (GET "/player/:name" [name] (get-player name)))
+  (GET "/player/:name" req (get-player req)))
